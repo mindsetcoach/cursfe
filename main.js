ec6b26eb4e80a360b53a276cd2ba122ff1954146
Tudor Goia <mindsetcoach360@gmail.com>
2021-11-16 21:22:47 +0200

    dupa curs 4

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log('salut');
+console.log('tuturor');
+console.log('salut');
+console.log('salut2');
+console.log('tuturor2');
+console.log('salut2');
+
+
+let a = 4;
+let persoana = 'tudor';
+let altaVariabila = a;
+
+console.log(altaVariabila);
+
+altaVariabila = 5;
+
+console.log(altaVariabila);
+
+altaVariabila = 5 * 3;
+
+console.log(altaVariabila);
+
+persoana = 'tudor' + ' dan';
+
+console.log(persoana);
+
+persoana = persoana + ' goia';
+
+console.log(persoana);
+
+alert(persoana);
