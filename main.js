19d24f3b84479dcd75d847cd4c48d0656c091b2a
Tudor Goia <mindsetcoach360@gmail.com>
2021-11-23 19:52:48 +0200

    some more play with objects arrays and data types

--- main.js
+++ main.js
@@ -1,31 +1,91 @@
-console.log('salut');
-console.log('tuturor');
-console.log('salut');
-console.log('salut2');
-console.log('tuturor2');
-console.log('salut2');
+// console.log('salut');
+// console.log('tuturor');
+// console.log('salut');
+// console.log('salut2');
+// console.log('tuturor2');
+// console.log('salut2');
 
 
 let a = 4;
 let persoana = 'tudor';
 let altaVariabila = a;
 
-console.log(altaVariabila);
+// now we'll show our variable to the console
+// console.log(altaVariabila);
 
 altaVariabila = 5;
 
-console.log(altaVariabila);
+// console.log(altaVariabila);
 
 altaVariabila = 5 * 3;
 
-console.log(altaVariabila);
+// console.log(altaVariabila);
 
 persoana = 'tudor' + ' dan';
 
-console.log(persoana);
+// console.log(persoana);
 
 persoana = persoana + ' goia';
 
-console.log(persoana);
+// console.log(persoana);
 
-alert(persoana);
+// alert(persoana);
+
+let justAnotherOne = 5;
+justAnotherOne = justAnotherOne + 1;
+justAnotherOne = justAnotherOne + 1;
+justAnotherOne = justAnotherOne + 1;
+
+justAnotherOne += 30;
+justAnotherOne += 30;
+justAnotherOne += 30 + 10;
+
+justAnotherOne = justAnotherOne - 10;
+justAnotherOne -= 5;
+justAnotherOne -= 5;
+justAnotherOne -= 5;
+
+console.log(203 / 20);
+
+console.log(203 % 20);
+
+console.log(typeof justAnotherOne);
+
+let aStringVar = 'just a string variable';
+
+console.log(typeof aStringVar);
+
+let myObject = {
+    name: 'my first object',
+    color: 'red',
+    age: 20
+};
+
+// console.log(typeof myObject);
+// console.log(myObject.toString());
+
+let myFirstArray = [4, 5, 3, 'abc', 5, {a: 11}];
+
+// console.log(typeof myFirstArray);
+// console.log(myFirstArray.toString());
+
+// console.log(justAnotherOne);
+
+// window.alert(myFirstArray);
+
+let myFirstBool;
+// console.log(typeof myFirstBool);
+
+myFirstBool = false;
+// console.log(typeof myFirstBool);
+
+// console.log(typeof myFirstBool);
+
+console.log(myFirstArray.length);
+console.log(myFirstArray[2]);
+console.log(myFirstArray[3]);
+console.log(myFirstArray[5]);
+console.log (myFirstArray[5].a );
+
+console.log(myObject.name);
+console.log(myObject.age);
