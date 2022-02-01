9e3204a56c4bdd440dfa65fc48015205c7ccd3cb
Tudor Goia <mindsetcoach360@gmail.com>
2022-02-01 19:01:47 +0200

    fixed icon and long algorithm issues

--- launches/utils.js
+++ launches/utils.js
@@ -22,18 +22,7 @@ let utils = {
 
     getLaunchesPerMonthFromAllLaunches: function(allLaunches) {
 
-        let l1 = 0;
-        let l2 = 0;
-        let l3 = 0;
-        let l4 = 0;
-        let l5 = 0;
-        let l6 = 0;
-        let l7 = 0;
-        let l8 = 0;
-        let l9 = 0;
-        let l10 = 0;
-        let l11 = 0;
-        let l12 = 0;
+        let launchesPerMonth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
         for (let i = 0; i < allLaunches.length; i++) {
 
@@ -41,21 +30,13 @@ let utils = {
             let dateUTC = aLaunch.date_utc;
             let monthPart = dateUTC.split('-')[1];
 
-            if (monthPart === '01') l1++;
-            if (monthPart === '02') l2++;
-            if (monthPart === '03') l3++;
-            if (monthPart === '04') l4++;
-            if (monthPart === '05') l5++;
-            if (monthPart === '06') l6++;
-            if (monthPart === '07') l7++;
-            if (monthPart === '08') l8++;
-            if (monthPart === '09') l9++;
-            if (monthPart === '10') l10++;
-            if (monthPart === '11') l11++;
-            if (monthPart === '12') l12++;
+            let monthAsNumber = parseInt(monthPart);
+
+            launchesPerMonth[monthAsNumber - 1]++;
+
         }
 
-        return [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12];
+        return launchesPerMonth;
     }
 
 };
